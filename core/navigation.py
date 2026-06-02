6019cd2f7f2feb96cd9711cd8edc79a76257bb93
SHIVAM ITCS <vijay.paliwal@outlook.com>
2026-06-02 17:33:42 +0530

    Update navigation.py

--- core/navigation.py
+++ core/navigation.py
@@ -1,1 +1,9 @@
+class NavigationEngine:
+    def open_invoicing_module(self, page):
+        pass
 
+    def apply_posted_filter(self, page):
+        pass
+
+    def locate_target_invoice(self, page):
+        pass

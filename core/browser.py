fed0c84241d26efacbcfc947fad1ecc9053ff674
SHIVAM ITCS <vijay.paliwal@outlook.com>
2026-06-02 17:33:28 +0530

    Update browser.py

--- core/browser.py
+++ core/browser.py
@@ -1,1 +1,14 @@
+from playwright.sync_api import sync_playwright
 
+
+class BrowserManager:
+    def launch(self):
+        playwright = sync_playwright().start()
+
+        browser = playwright.chromium.launch(
+            headless=True
+        )
+
+        context = browser.new_context()
+
+        return playwright, browser, context
